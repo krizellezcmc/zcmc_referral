4a17065147be6b9d27e0ace9aa05074aaa47838d
krizellezcmc <kmfalcasantos.zcmc@gmail.com>
2022-09-16 10:09:06 +0800

    krize

--- src/API/Api.js
+++ src/API/Api.js
@@ -1,11 +1,11 @@
 import axios from "axios";
-axios.defaults.withCredentials = true;
+// axios.defaults.withCredentials = true;
 
 let serverUrl = "https://zcmc-referral-online.preview-domain.com/api/";
 
 //Custom API to fetch data from the server using axios
 const api = axios.create({
-  withCredentials: true,
+  // withCredentials: true,
   baseURL: serverUrl,
   headers: {
     Authorization: `${JSON.parse(sessionStorage.getItem("sessionId"))}`,
