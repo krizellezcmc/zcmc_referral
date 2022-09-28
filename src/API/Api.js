6ea5d7ff87e7115935937a954e78a421d141a4ce
krizellezcmc <kmfalcasantos.zcmc@gmail.com>
2022-09-28 09:34:06 +0800

    krizelle

--- src/API/Api.js
+++ src/API/Api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 // axios.defaults.withCredentials = false;
 
-// const server = "http://192.168.3.121/zcmc_referral_api/api/";
 const server = "http://192.168.3.121/zcmc_referral_api/api/";
+// const server = "http://192.168.3.121/zcmc_referral_api/api/";
 // const server = "https://zcmc-referral.herokuapp.com/api/";
 
 //Custom API to fetch data from the server using axios
