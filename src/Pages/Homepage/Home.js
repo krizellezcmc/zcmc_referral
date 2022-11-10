d97651b330616c6a84161e4057e91fe2d8c69897
Alyana Barretto <alyanabarretto.zcmc@gmail.com>
2022-11-10 09:14:35 +0800

    yana

--- src/Pages/Homepage/Home.js
+++ src/Pages/Homepage/Home.js
@@ -13,12 +13,15 @@ import {
   Link,
   Spacer,
   Text,
+  IconButton,
+  flexbox,
 } from "@chakra-ui/react";
 import { useNavigate, useLocation } from "react-router-dom";
-import { BiUpArrowAlt } from "react-icons/bi";
+import { BiX, BiMenu, BiUpArrowAlt } from "react-icons/bi";
 
 function Home(props) {
   const [showScrollUp, setShowScrollUp] = useState(false);
+  const [display, changeDisplay] = useState("none");
   const navigate = useNavigate();
   const location = useLocation();
 
@@ -49,41 +52,61 @@ function Home(props) {
   return (
     <>
       <Box width="100%">
-        <Box shadow="sm" className="nav" bgColor="white">
+        <Flex shadow="sm" className="nav" bgColor="white">
           <Container maxW="container.xl">
             <Flex py={4} align="center">
               <Text fontWeight={800} fontSize="15px">
                 One Hospital Command
               </Text>
               <Spacer />
-              <div className="nav-list">
-                <Link
-                  className={location.hash == "#header" ? "active" : "nav-item"}
-                  href="#header "
-                >
-                  Home
-                </Link>
-                <Link
-                  className={
-                    location.hash == "#services" ? "active" : "nav-item"
-                  }
-                  href="#services"
-                >
-                  Services
-                </Link>
-                <Link
-                  className={location.hash == "#about" ? "active" : "nav-item"}
-                  href="#about"
-                >
-                  About
-                </Link>
-                <Link
-                  className={location.hash == "#faqs" ? "active" : "nav-item"}
-                  href="#faqs"
-                >
-                  FAQs
-                </Link>
-              </div>
+              <Flex display={{ sm: "none", md: "flex", lg: "flex" }}>
+                <div className="nav-list">
+                  <Link
+                    className={
+                      location.hash == "#header"
+                        ? "nav-item active"
+                        : "nav-item"
+                    }
+                    href="#header "
+                  >
+                    Home
+                  </Link>
+                  <Link
+                    className={
+                      location.hash == "#services"
+                        ? "nav-item active"
+                        : "nav-item"
+                    }
+                    href="#services"
+                  >
+                    Services
+                  </Link>
+                  <Link
+                    className={
+                      location.hash == "#about" ? "nav-item active" : "nav-item"
+                    }
+                    href="#about"
+                  >
+                    About
+                  </Link>
+                  <Link
+                    className={
+                      location.hash == "#faqs" ? "nav-item active" : "nav-item"
+                    }
+                    href="#faqs"
+                  >
+                    FAQs
+                  </Link>
+                </div>
+              </Flex>
+              <IconButton
+                aria-label="Open Menu"
+                size="lg"
+                mr={2}
+                icon={<BiMenu />}
+                display={{ sm: "flex", md: "none", lg: "none" }}
+                onClick={() => changeDisplay("flex")}
+              />
               <Button
                 size="sm"
                 rounded="full"
@@ -94,8 +117,69 @@ function Home(props) {
                 Sign in
               </Button>
             </Flex>
+            <Flex
+              w="100vw"
+              bgColor="gray.100"
+              zIndex={20}
+              h="100vh"
+              pos="fixed"
+              top="0"
+              left="0"
+              overflow="auto"
+              flexDir="column"
+              display={display}
+            >
+              <Flex justify="flex-end">
+                <IconButton
+                  mt={2}
+                  mr={2}
+                  aria-label="Close Menu"
+                  size="lg"
+                  icon={<BiX />}
+                  onClick={() => changeDisplay("none")}
+                />
+              </Flex>
+              <Flex flexDir="column" align="center">
+                <Link
+                  className={
+                    location.hash == "#header" ? "active" : "nav-item2"
+                  }
+                  href="#header "
+                  my={5}
+                  onClick={() => changeDisplay("none")}
+                >
+                  Home
+                </Link>
+                <Link
+                  className={
+                    location.hash == "#services" ? "active" : "nav-item2"
+                  }
+                  href="#services"
+                  my={5}
+                  onClick={() => changeDisplay("none")}
+                >
+                  Services
+                </Link>
+                <Link
+                  className={location.hash == "#about" ? "active" : "nav-item2"}
+                  href="#about"
+                  my={5}
+                  onClick={() => changeDisplay("none")}
+                >
+                  About
+                </Link>
+                <Link
+                  className={location.hash == "#faqs" ? "active" : "nav-item2"}
+                  href="#faqs"
+                  my={5}
+                  onClick={() => changeDisplay("none")}
+                >
+                  FAQs
+                </Link>
+              </Flex>
+            </Flex>
           </Container>
-        </Box>
+        </Flex>
 
         <div className="content">
           {showScrollUp && (
