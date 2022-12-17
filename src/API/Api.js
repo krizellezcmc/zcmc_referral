2eeae4bd78fb679916bf7f8bbaf4ce796d493c95
krizellezcmc <kmfalcasantos.zcmc@gmail.com>
2022-12-17 08:35:43 +0800

    krizelle

--- src/API/Api.js
+++ src/API/Api.js
@@ -2,13 +2,14 @@ import axios from "axios";
 // axios.defaults.withCredentials = false;
 
 // const server = "https://onehospital.online/api/";
-const server = "http://192.168.3.135/zcmc_referral_api_hosted/api/";
+// const server = "http://192.168.3.135/zcmc_referral_api_hosted/api/";
 // const server = "http://192.168.3.121/zcmc_referral_api/api/";
 // const server = "https://zcmc-referral.herokuapp.com/api/";
+const server = "https://zcmc-patientreferral.online/api/";
 
 //Custom API to fetch data from the server using axios
 const api = axios.create({
-  // withCredentials: true,
+  // withCredentials: true,s
   baseURL: server,
   headers: {
     Authorization: `${JSON.parse(sessionStorage.getItem("sessionId"))}`,
