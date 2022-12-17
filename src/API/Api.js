c7d0bd96d2ee118bc793a7561d4a6eb2bf884b09
krizellezcmc <kmfalcasantos.zcmc@gmail.com>
2022-12-17 09:49:45 +0800

    kriz

--- src/API/Api.js
+++ src/API/Api.js
@@ -5,7 +5,7 @@ import axios from "axios";
 // const server = "http://192.168.3.135/zcmc_referral_api_hosted/api/";
 // const server = "http://192.168.3.121/zcmc_referral_api/api/";
 // const server = "https://zcmc-referral.herokuapp.com/api/";
-const server = "https://zcmc-patientreferral.online/api/";
+const server = "https://referralserver.online/api";
 
 //Custom API to fetch data from the server using axios
 const api = axios.create({
